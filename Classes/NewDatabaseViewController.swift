81f461b1bdcd1f11ad0ac07491a5fa0dd104b394
Chad Austin <chad@chadaustin.me>
2019-09-13 12:18:11 -0700

    open project with Xcode 10.3

--- Classes/NewDatabaseViewController.swift
+++ Classes/NewDatabaseViewController.swift
@@ -119,7 +119,7 @@ class NewDatabaseViewController: NetworkActivityViewController, UITextFieldDeleg
     func uploadTemplate() {
         let path = Bundle.main.path(forResource: "template", ofType: "kdb")!
         let reader = KdbReader(kdbFile: path, usingPassword: "password")
-        if reader.hasError != .some(false) {
+        if reader.hasError {
             networkRequestStopped()
             let error = UIAlertView(title: "Error", message: "There was a fatal error loading the database template. You may need to reinstall PassDrop.", delegate: nil, cancelButtonTitle: "Cancel")
             error.show()
